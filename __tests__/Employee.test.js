7cacdc2e795d0853edb710feb58843ba8bdf5654
Andrew Ellison <acannonellison@gmail.com>
2021-11-17 09:43:55 -0800

    Created basic tests for employee class.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,92 @@
+const Employee = require("../lib/Employee")
+
+describe('Initialization', () => {
+  it('Should set class variables when constructed', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.name).toBe('Andrew');
+    expect(emp.id).toBe(1);
+    expect(emp.email).toBe('a@a.com');
+  });
+
+  it('Should input default values when constructed without input', () => {
+    const emp = new Employee();
+
+    expect(emp.name).toBe("");
+    expect(emp.id).toBe(0);
+    expect(emp.email).toBe("");
+  });
+
+  it('Should create a default query list when constructed', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.queryList[0]).toEqual(["name",`Enter ${emp.getRole()}'s name:`,"string"],);
+    expect(emp.queryList[1]).toEqual(["id",`Enter ${emp.getRole()}'s ID number:`,"integer"],);
+    expect(emp.queryList[2]).toEqual(["email",`Enter ${emp.getRole()}'s email:`,"string"],);
+  });
+});
+
+describe('Getters', () => {
+  it('Should return the employee name', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.getName()).toEqual('Andrew');
+  })
+
+  it('Should return the employee ID', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.getId()).toEqual('ID: 1');
+  })
+
+  it('Should return the employee email', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.getEmail()).toEqual('Email: <a href="mailto:a@a.com?Subject=Hello%20Employee">a@a.com</a>');
+  })
+
+  it('Should return the employee role', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.getRole()).toEqual("Employee");
+  })
+
+  it('Should return the employee auxiliary value of "" for a base employee', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    expect(emp.getAux()).toEqual("");
+  })
+
+  it('Should return the employee card html', () => {
+    const emp = new Employee('Andrew',1,'a@a.com');
+
+    let expectedCard = `<div class="card m-2 flex-fill">
+  <div class="card-header bg-info">
+    <h2>Andrew</h2>
+    <h3>Employee</h3>
+  </div>
+  <div class="card-body p-3 bg-light">
+    <ul class="list-group">
+      <li class="list-group-item">ID: 1</li>
+      <li class="list-group-item">Email: <a href="mailto:a@a.com?Subject=Hello%20Employee">a@a.com</a></li>
+      <li class="list-group-item"></li>
+    </ul>
+  </div>
+</div>`;
+
+  expect(emp.getCard()).toEqual(expectedCard);
+
+  })
+
+});
+
+describe('Queries', () => {
+
+  // Must mock inquirer responses
+  //Need new path
+  //jest.mock('../inquirer');
+
+
+  
+
+});
